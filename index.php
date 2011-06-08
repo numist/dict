f578128127742034f6c2ef036408488a666369a4
Scott Perry <numist@numist.net>
2011-06-08 09:44:33 -0700

    Some bugfixes, some debugging.
    • fixed an issue where a word with only one (or no) result would not update the defs.
    • better debugging.
    • XMLReader doesn't seem to validate unless there is a DTD present. oof.
    • switching to attribute callbacks instead of attribute draining does not work—the attribute callback is never called, even in the simple case. (?!)

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php
 
-//define("DEBUG", true);
+define("DEBUG", true);
+error_reporting(E_ALL | E_STRICT);
+
+function indent($line, $level) {
+  $str = "";
+  while($level-- > 0) {
+    $str .= "  ";
+  }
+  return $str.trim($line);
+}
+
+function format_human($str) {
+  $str = str_replace(array("<", ">"), array("\n<", ">\n"), $str);
+  $str = str_replace("\n\n", "\n", $str);
+  $arr = explode("\n", $str);
+  $level = 0;
+  for($i = 0; $i < count($arr); $i++) {
+    if(substr($arr[$i], 0, 2) == "</") {
+      $level--;
+      $arr[$i] = indent($arr[$i], $level);
+    } else if(substr($arr[$i], 0, 1) == "<" && substr($arr[$i], 0, 5) != "<meta" && substr($arr[$i], 0, 2) != "<!") {
+      $arr[$i] = indent($arr[$i], $level);
+      $level++;
+    } else {
+      $arr[$i] = indent($arr[$i], $level);
+    }
+  }
+  return implode("\n", $arr);
+}
 
 require("lib/dictionary.class");
 
